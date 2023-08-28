f41d716374daf204e19c169ce1cfea6d36f769bd
KeYi <46948667+bean4896@users.noreply.github.com>
2023-08-28 15:13:24 +0800

    update js and css

--- gosu.js
+++ gosu.js
@@ -1,1 +1,18 @@
-console.log('gosu.js')
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    freeMode: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
